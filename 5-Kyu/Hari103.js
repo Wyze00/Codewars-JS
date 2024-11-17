ef24d4b9f932534ff1ce9973cf2a379dc60dbea9
Wyze69 <nathantanusubroto15@gmail.com>
2024-11-17 17:31:29 +0700

    Hari 103

--- 5-Kyu/Hari103.js
+++ 5-Kyu/Hari103.js
@@ -1,1 +1,36 @@
-// (347)
+// Is my friend cheating? (347)
+
+function removeNb (n) {
+
+    const seq = Array.from({length: n+1}).map((_,i) => i);
+
+    let start = 1;
+    let end = n;
+
+    let sum = seq.reduce((a,c) => a + c,0) - seq[start] - seq[end];
+    let aXb = seq[start] * seq[end];
+
+    while(aXb != sum){
+
+        if(start == end){return []}
+
+        if(aXb < sum){
+            start++;
+
+            aXb = seq[start] * seq[end];
+            sum = sum + seq[start-1] - seq[start];
+            continue;
+
+        } else {
+            end--;
+
+            aXb = seq[start] * seq[end];
+            sum = sum - seq[end] + seq[end+1];
+            // continue;
+        }
+    }
+
+    return [[seq[start],seq[end]],[seq[end],seq[start]]]
+}
+
+console.log(removeNb(26));
