4c8f6cf1c23cce988a91371799d8fd3b94610162
Wyze69 <nathantanusubroto15@gmail.com>
2024-12-03 13:46:58 +0700

    +

---
+++ 6-Kyu/Hari114.js
@@ -0,0 +1,30 @@
+// Two Joggers (366)
+
+var nbrOfLaps = function (x, y) {
+
+    for(let i = Math.min(x,y); i>1; i--){
+
+        while(x%i == 0 && y%i == 0){
+            x /= i;
+            y /= i;
+        }
+    }
+
+    return [y,x]
+}
+
+// Autocomplete! Yay!
+
+function autocomplete(input, dictionary){
+
+    const arr =  dictionary.filter((e) => {
+
+        const temp = e.replace(/[^a-zA-Z1234567890]/g,'');
+
+        return temp.match(input) != null;
+
+    })
+    
+    return arr;
+}
+
