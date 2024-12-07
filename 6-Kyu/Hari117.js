a2cc767942082c91ec192ee2cad0996ea0f4145f
Wyze69 <nathantanusubroto15@gmail.com>
2024-12-07 13:02:24 +0700

    +

--- 6-Kyu/Hari117.js
+++ 6-Kyu/Hari117.js
@@ -4,3 +4,18 @@ function wordCount(s) {
   return s.split(/[^A-Za-z]/).filter((e) => !e.match(/\b(a|the|on|at|of|upon|in|as)\b/gi) && e).length;
 }
 
+// Simple Fun #218: Replace Words (372)
+
+function replaceWords(sentence) {
+
+    sentence = sentence.split(' ');
+    const sorted  = sentence.concat().sort((a,b) => a.length-b.length);
+    const res = new Array(sentence.length);
+
+    for(let i = 0, j = sentence.length-1; i<sentence.length; i++, j--){
+        res[sentence.indexOf(sorted[i])] = sorted[j] == 'I' ? 'I' : sorted[j].toLowerCase();
+    }
+
+    res[0] = res[0][0].toUpperCase()+res[0].slice(1)
+    return res.join(' ')
+}
