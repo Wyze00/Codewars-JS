92ee9abfc2e679485da5ec461d25e13b30140d8d
Wyze69 <nathantanusubroto15@gmail.com>
2024-12-07 14:31:14 +0700

    +

--- 6-Kyu/Hari117.js
+++ 6-Kyu/Hari117.js
@@ -19,3 +19,44 @@ function replaceWords(sentence) {
     res[0] = res[0][0].toUpperCase()+res[0].slice(1)
     return res.join(' ')
 }
+
+
+// Square into Squares. Protect trees! (373)
+
+
+function decompose(n) {
+
+    const aja = [];
+
+    for (let i = n - 1; i > 0; i--) {
+        const arr = [i];
+
+        if (another(Math.floor(Math.sqrt(n ** 2 - i ** 2)), (n ** 2 - i ** 2), arr.concat())) {
+            return aja[0];
+        }
+    }
+
+    return null;
+
+    function another(n, rem, arr) {
+        if (rem === 0) {
+            aja.push(arr);
+            return true; 
+        }
+
+        for (let i = n; i > 0; i--) {
+
+            if (arr.includes(i) || arr[0] < i) {
+                break;
+            }
+
+            const temp = arr.concat();
+            temp.unshift(i);
+
+            if (another(Math.floor(Math.sqrt(rem - i ** 2)), (rem - i ** 2), temp)) {
+                return true; 
+            }
+        }
+        return false; 
+    }
+}
