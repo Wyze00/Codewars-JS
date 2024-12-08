e83a7c005c8ac84a1386a09731c60ba35267f15a
Wyze69 <nathantanusubroto15@gmail.com>
2024-12-08 16:19:45 +0700

    +

--- 4-Kyu/Hari118.js
+++ 4-Kyu/Hari118.js
@@ -1,4 +1,4 @@
-// Sum Strings as Numbers (374);
+// Sum Strings as Numbers (374)
 
 function sumStrings(a,b) { 
 
@@ -50,4 +50,78 @@ function sumStrings(a,b) {
     }
 
     return result;
+}
+
+// Strings Mix (375)
+
+function mix(s1, s2) {
+
+    const letter1 = s1.match(/[a-z]/g);
+    const letter2 = s2.match(/[a-z]/g);
+
+    const myObj1 = {};
+    const myObj2 = {};
+
+    for(let i = 0; i<letter1.length; i++){
+
+        if(myObj1[letter1[i]]){
+            myObj1[letter1[i]]++;
+        } else {
+            myObj1[letter1[i]] = 1;
+        }
+    }
+
+    for(let i = 0; i<letter2.length; i++){
+
+        if(myObj2[letter2[i]]){
+            myObj2[letter2[i]]++;
+        } else {
+            myObj2[letter2[i]] = 1;
+        }
+    }
+
+    const result = [];
+
+    for(const letter in myObj1){
+
+        const l1 = myObj1[letter];
+        const l2 = myObj2[letter];
+
+        if(l1 == 1 && l2 == 1){
+            continue;
+        }
+
+        if(l2 == undefined && l1 > 1 || l1 > l2){
+            result.push(['1', ''.padEnd(l1, letter)]);
+            continue;
+        } else if(l1 > l2){
+            result.push(['1', ''.padEnd(l1, letter)]);
+        } else if(l1 == l2) {
+            result.push(['=', ''.padEnd(l1, letter)]);
+        } else if(l2 > l1){
+            result.push(['2', ''.padEnd(l2, letter)]);
+        }  
+    }
+
+    for(const letter in myObj2){
+
+        const l2 = myObj2[letter];
+
+        if(myObj1[letter] == undefined && l2 > 1){
+            result.push(['2', ''.padEnd(l2, letter)]);  
+        }
+    }
+
+    result.sort((a,b) => {
+
+        if(a[1].length == b[1].length && a[0] == b[0]){
+            return a[1][0].charCodeAt()-b[1][0].charCodeAt();
+        } else if(a[1].length == b[1].length){
+            return a[0].charCodeAt()-b[0].charCodeAt();
+        }
+
+        return b[1].length-a[1].length;
+    });
+
+    return result.map((e) => e.join(':')).join('/');
 }
