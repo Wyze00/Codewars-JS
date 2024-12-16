4fe6c15b8a4afdc08f30b40697e9d73317bb9474
Wyze69 <nathantanusubroto15@gmail.com>
2024-12-16 19:59:59 +0700

    +

--- 6-Kyu/Hari124.js
+++ 6-Kyu/Hari124.js
@@ -1,1 +1,14 @@
-// (384)
+// Financing Plan on Planet XY140Z-n (384)
+
+function finance(n) {
+
+    let res = 0;
+ 
+    for(let i = 0, j = 0, k = 0; i<=n; i++, j+=3, k += j){
+         res += k;
+     }
+     
+     return res;
+ }
+
+ 
