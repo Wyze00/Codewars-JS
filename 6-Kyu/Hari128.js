177991a0bf7b5e44d4ee6367254e773ffa619691
Wyze69 <nathantanusubroto15@gmail.com>
2024-12-20 16:57:44 +0700

    +

--- 6-Kyu/Hari128.js
+++ 6-Kyu/Hari128.js
@@ -10,7 +10,37 @@ function egged(year,span) {
     return eT == 0 ? 'No chickens yet!' : eT;
 }
 
+// Closest Sum (392)
 
+const closestSum = (ints, num) => {
+
+    const res = [];
+
+    check(ints, 0, 3);
+
+    function check(arr, sum, depth){
+
+        if(depth == 0){
+            res.push(sum);
+            return;
+        }
+
+        for(let i = 0; i<arr.length-depth+1; i++){
+            check(arr.concat().slice(i+1), sum + arr[i], depth - 1);
+        }
+    }
+
+    return res.sort((a,b) => {
+        
+        const dif = Math.abs(a-num)-Math.abs(b-num)
+
+        if(dif == 0){
+            return a-b;
+        }
+
+        return dif;
+    })[0];
+}
 
 
  
