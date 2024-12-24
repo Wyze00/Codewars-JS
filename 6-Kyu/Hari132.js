3386d722b4fc492c23988e150a86ded878dc1e6f
Wyze69 <nathantanusubroto15@gmail.com>
2024-12-24 13:21:06 +0700

    +

--- 6-Kyu/Hari132.js
+++ 6-Kyu/Hari132.js
@@ -21,3 +21,35 @@ function oneCharDifference(s1, s2) {
 }
 
 
+// Increment with iterations (400)
+
+function increment(number, iterations, spacer) {
+
+    const num = number.toString().split('').map(e=>~~e);
+    let len = num.length;
+
+    for(let i = 1, j = spacer%num.length ; i<=iterations; i++, j = (j + spacer) % num.length){   
+
+        num[j]++;
+
+        for(let i = num.length-1; i > 0; i--){
+
+            if(num[i] > 9){
+                num[i-1]++;
+                num[i] = 0;
+            }
+        }
+
+        while(num[0] > 9){
+            num.unshift(1);
+            num[1] = 0;
+        }
+
+        if(num.length > len){
+            j++;
+            len++;
+        }
+    }
+
+    return Number(num.join(''));
+}
