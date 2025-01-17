2827396405409eec9c69ddbc9550d4fe3a150bb0
Wyze69 <nathantanusubroto15@gmail.com>
2025-01-17 15:54:17 +0700

    +

--- 6-Kyu/Hari149.js
+++ 6-Kyu/Hari149.js
@@ -12,3 +12,38 @@ function maxSum(arr,range){
     
     return max;
 }
+
+// * Custom Christmas Tree (424)
+
+function customChristmasTree(chars,n){
+  
+    let res = ``;
+    let ctr = 0;
+
+    for(let i = 1; i<=n; i++){
+
+        for(let j = n-i; j>0; j--){
+            res += " ";
+        }
+
+        for(let j = 0; j<i; j++){
+            res += chars[ctr % chars.length];
+            if(j != i-1) res += " ";
+            ctr++;
+        }
+
+        res += "\n";
+    }
+
+    for(let i = 0; i<Math.floor(n/3); i++){
+        
+        for(let i = 1; i<=n-1; i++){
+            res += " ";
+        }
+
+        res += "|"
+        if(i != Math.floor(n/3)-1) res += "\n";
+    }
+
+    return res;
+}
