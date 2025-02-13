475addba5515500fdc06024d234c2a5258bc62ad
Wyze69 <nathantanusubroto15@gmail.com>
2025-02-13 14:33:12 +0700

    +

---
+++ 6-Kyu/Hari171.js
@@ -0,0 +1,26 @@
+// * Scaling Squared Strings (438)
+
+
+function scale(strng, k, n) {
+
+    const words = strng.split("\n");
+    const res = [];
+
+    for(let i = 0; i<words.length; i++){
+
+        let str = "";
+
+        for(let j = 0; j<words[i].length; j++){
+
+            for(let l = 0; l<k; l++){
+                str += words[i][j];
+            }
+        }
+
+        for(let j = 0; j<n; j++){
+            res.push(str);
+        }
+    }
+
+    return res.join("\n");
+}
